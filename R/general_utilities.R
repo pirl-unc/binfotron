721af88f55c983a7c86f929abc4076e09ead29e7
Dante S Bortone <dbortone@email.unc.edu>
2022-11-10 11:16:08 -0500

    Added several functions to general_functions.

--- R/general_utilities.R
+++ R/general_utilities.R
@@ -410,3 +410,252 @@ write_metadata = function(
   }
 }
 
+
+
+
+#' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' import_model_data
+#' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title Perform common steps needed for importing model data
+#' 
+#' @description
+#' All models will start with a matrix of ranked features, and the feature
+#' values of the features. This fxn orders the features, pulls out the top x and
+#' outputs disc and validation dataset matrices with the correct indep and dep
+#' variables. This function has the option to specify certain features or to pull
+#'the top x feature from a columns ranks file.
+#'  
+#' @param ranks_file_pattern String to use to look for a ranking of features
+#' @param ranks_clm String to indicate which column contains the column ranks
+#' @param ranks_count Integer to indicate houw many features to take from the column ranks
+#' @param merge_file_patterns Character vector of file patterns to look for and merge to make the final output matrices
+#' @param set_clm Column name to indicate which samples are in "Validation" vs "Discovery" sets
+#' @param sample_key Column nameto use as the sample key to merge all matrices
+#' @param indep_vars Character string of column names for the independent variables
+#' @param dep_vars Character string of column names for the dependent variables
+#' 
+#' @export
+import_model_data = function(
+  ranks_file_pattern = "_ranks.tsv$",
+  ranks_clm = NULL,
+  ranks_count = NULL,
+  merge_file_patterns = NULL,
+  set_clm = "Set",
+  sample_key = "Run_ID",
+  indep_vars = NULL,
+  dep_vars = NULL
+){
+
+  if (!is.null(indep_vars) & (!is.null(ranks_file_pattern) | !is.null(ranks_count) | !is.null(ranks_clm)))
+    stop("Ranks arguments and indep_vars should not be set at the same time.")
+
+  if (!is.null(ranks_count)){
+    if (is.null(ranks_file_pattern)){
+      stop("ranks_file_pattern cannot be NULL if using ranks_count.")
+    } else {
+      ranks_feature_path = list.files(pattern = ranks_file_pattern)
+      if (length(ranks_feature_path) == 0) stop(paste0("Could not file with ranks_file_pattern: ", ranks_file_pattern))
+      if (length(ranks_feature_path) > 1 ) stop(paste0("Found multiple files for ranks_file_pattern: ", ranks_file_pattern))
+      ranks_df = fread(ranks_feature_path, data.table = F)
+      if (is.null(ranks_clm)) ranks_clm = names(ranks_df)[2]
+      ranks_cls = class(ranks_df[[ranks_clm]])
+      if (ranks_cls %in% c('numeric', 'integer')){
+        ranks_df = ranks_df[order(ranks_df[[ranks_clm]]),]
+      } else if (ranks_cls %in% c('logical')) {
+        ranks_df = ranks_df[rev(order(ranks_df[[ranks_clm]])),]
+      } else {
+        stop(paste0("Unrecognized class for the ", ranks_clm," ranks_clm: ", ranks_cls))
+      }
+      if (is.null(ranks_count)) stop("ranks_count must be set if ranks are being used.")
+      indep_vars = ranks_df[[1]][1:ranks_count]
+    }
+  }
+
+  cat(paste0("dep_vars: ", paste0( dep_vars, collapse= ", "), "\n"))
+  cat(paste0("indep_vars: ", paste0( indep_vars, collapse= ", "), "\n"))
+  all_clms = c(sample_key, set_clm, dep_vars, indep_vars)
+  if (any(duplicated(all_clms))){
+    duplicated_clms = all_clms[duplicated(all_clms)]
+    message(paste0("duplicated_clms: ", paste0( duplicated_clms, collapse= ", ")))
+    stop("You have duplicated column names specified in your set_clm, sample_key, indep_vars, and dep_vars. Please resolve this.")
+  }
+
+  import_paths = c()
+  for (merge_file_pattern in merge_file_patterns){
+    import_paths %<>% c(grep(merge_file_pattern, my_paths, value = T))
+  }
+  if ( length(import_paths) == 0 ) stop("No files with the merge_file_patterns could be found")
+
+  # limit to varaibles that are in all_clms
+  import_df = NULL
+  for (import_path in import_paths){
+    this_df = fread(import_path, data.table = F)
+    if (!sample_key %in% names(this_df)) stop(paste0("This path does not contain your sample_key, ", sample_key,": ", import_path))
+    this_df = this_df[, all_clms[all_clms %in% names(this_df)]]
+    if (ncol(this_df) == 1) stop(paste0("This file did not have any data you were looking for: ", import_path))
+    if(is.null(import_df)){
+      import_df = this_df
+    } else {
+      import_df = merge(import_df, this_df, by = sample_key, all = T)
+    }
+  }
+
+  missing_clms = c(set_clm, dep_vars, indep_vars)[!(c(set_clm, dep_vars, indep_vars) %in% names(import_df))]
+
+  if (length(missing_clms) > 0) stop(paste0(c("Your imported data are missing the following columns:", missing_clms), collapse= "\n"))
+  if (!is.null(set_clm)){
+    if (!all(c("Validation","Discovery") %in% import_df[[set_clm]])) stop(paste0("Your set_clm, ", set_clm, ", does not contain 'Discovery' and 'Validation' strings."))
+    val_df = import_df[import_df[[set_clm]] == "Validation", ]
+    import_df = import_df[import_df[[set_clm]] == "Discovery", ]
+    val_df = val_df[, names(val_df)[names(val_df) != set_clm]]
+    import_df = import_df[, names(import_df)[names(import_df) != set_clm]]
+  } else {
+    val_df = NULL
+  }
+
+  # dataset prep is the right place to make sure the features aren't univariable for discovery validation sets, not here
+  return(list(disc_df=import_df, val_df=val_df, indep_vars=indep_vars))
+}
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# minimal_decimal_places_ordered
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title Reduce decimal places needed
+#' 
+#' @description
+#' Reduce the number of decimals down to the minimal needed to preserve the same number of levels in the original data. Returns data as factors to represent values in the same order they were given. 
+#' This is handy for whne you 
+#'  
+#' @param numeric_vector Values to represent
+#' @param min_decimal Integer to indicate the number of folds in which to split the data
+#' 
+#' @export
+minimal_decimal_places_ordered = function(numeric_vector, min_decimal = 0){
+	# min_decimal = 0
+	initial_levels =  unique(numeric_vector)
+	initial_level_count = length(initial_levels)
+	test_levels = unique(specify_decimal(initial_levels, min_decimal))
+	test_level_count = length(test_levels)
+
+	while(initial_level_count > test_level_count){
+		min_decimal = min_decimal + 1
+		test_levels = unique(specify_decimal(initial_levels, min_decimal))
+		test_level_count = length(test_levels)
+	}
+	numeric_vector %<>% factor(levels = sort(unique(.)))
+	levels(numeric_vector) %<>% specify_decimal(min_decimal)
+	return(numeric_vector)
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# balanced_folds
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title Get indices of a fold of data that balances the distribution of values.
+#' 
+#' @description
+#' This was written by Joel Parker.
+#'  
+#' @param y Vector to balance
+#' @param nfolds Integer to indicate the number of folds in which to split the data
+#' 
+#' @export
+balanced_folds<- function(y, nfolds = 3) { # from Joel
+	permuteRows = function(x) {
+		dd = dim(x)
+		n = dd[1]
+		p = dd[2]
+		mm = runif(length(x)) + rep(seq(n) * 10, rep(p, n))
+		matrix(t(x)[order(mm)], n, p, byrow = T)
+	}
+
+	totals = table(y)
+	fmax = max(totals)
+	nfolds = min(nfolds, fmax)
+	folds = as.list(seq(nfolds))
+	yids = split(seq(y), y)
+	bigmat = matrix(NA, ceiling(fmax / nfolds) * nfolds, length(totals))
+	for(i in seq(totals))
+		bigmat[seq(totals[i]), i] = sample(yids[[i]])
+	smallmat = matrix(bigmat, nrow = nfolds)
+	smallmat = permuteRows(t(smallmat))
+	res = vector("list", nfolds)
+	for(j in 1:nfolds) {
+		jj = !is.na(smallmat[ , j])
+		res[[j]] = smallmat[jj, j]
+	}
+
+	return(res)
+}
+
+# how is the t.test method different than the z test
+# https://sphweb.bumc.bu.edu/otlt/MPH-Modules/PH717-QuantCore/PH717-Module6-RandomError/PH717-Module6-RandomError11.html
+# https://www.statskingdom.com/doc_t_test_z_test.html
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# t_test_ci -  calculates mean & CI for residuals using t-distribution
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title calculates mean & CI for residuals using t-distribution
+#' 
+#' @description
+#' This CI method does not assume we know the sd and uses different t-distributions to get a better estimate of the distribution of the data.
+#' Both t_test_ci and z_test_ci assume the data are normally distributed. It will usually be better to use the t_test_ci. This will equal the z_test_ci as N -> inf
+#' 
+#' @param x Numeric vector to correlate with vector_b
+#' 
+#' @export
+t_test_ci = function(x){
+  my_mean = mean(x)
+	my_result = tryCatch({
+		my_ci = t.test(x)$conf.int
+		my_result = list(mean = my_mean, lower_ci = my_ci[1], upper_ci = my_ci[2])
+	}, error = function(e) {
+		my_mean = mean(x)
+		my_result = list(mean = my_mean, lower_ci = NA, upper_ci = NA)
+	})
+	return(my_result)
+}
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# z_test_ci -  calculates mean & CI for residuals using z-score sd
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title calculates mean & CI for residuals using z-score sd
+#' 
+#' @description
+#' The z test assumes we know the sd ( ie we have a large number of samples n > 30). Both t_test_ci and z_test_ci assume the data are normally distributed. It will usually be better to use the t_test_ci. This will equal the t_test_ci as N -> inf
+#'  
+#' @param x Numeric vector to correlate with vector_b
+#' @param show_warning Boolean to indicate whether a warning be shown if there are too few samples to accurately use this method to get confidence intervals
+#' 
+#' @export
+z_test_ci = function(x, show_warning = T) {
+  if (show_warning) {
+    if (length(x) < 30) {
+      warning("N is fewer than 30 values so a t-test would be more appropriate in this case. Try using t_test_ci")
+    }
+  }
+  mean = mean(x)
+  my_sd = sd(x)/sqrt(length(x))
+  lower_ci = (mean - (1.96 * my_sd))
+  upper_ci = (mean + (1.96 * my_sd))
+  return(list(mean = mean, lower_ci = lower_ci, upper_ci = upper_ci))
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# get_rho -  calculates Rho & CI
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @title Run cor.test and passes results 
+#'  
+#' @param vector_a Numeric vector to correlate with vector_b
+#' @param vector_b Numeric vector to correlate with vector_a
+#' @param my_method string to pass to cor.test: 'pearson' or 'spearman'
+#' 
+#' @export
+get_rho = function(vector_a, vector_b, my_method = "pearson") {
+  test_result = cor.test(vector_a, vector_b,
+                         alternative = "two.sided", method = my_method, exact = F)
+  conf_int = test_result$conf.int
+  my_rho = as.numeric(test_result$estimate)
+  return(list(rho = my_rho, lower_ci = conf_int[1], upper_ci = conf_int[2], pvalue = test_result$p.value))
+}
+  
